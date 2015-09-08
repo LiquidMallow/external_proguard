d1da646d685db7a2119e43a709c2830a2700bdcd
Dave Kessler <activethrasher00@gmail.com>
2015-09-08 00:59:37 -0400

    Upgrade to Proguard 5.2.1
    Change-Id: I88362c7339c67174fdbfcb4b70612e1f32e09058
    
    Fix setting proguard 5.2.1 for build
    
    Change-Id: Ie8b9b295d59ebd0fb0483387070ec7152933f53d

--- bin/proguard.sh
+++ bin/proguard.sh
@@ -22,5 +22,5 @@ fi
 PROGUARD_HOME=`dirname "$PROGUARD"`/..
 
 # BEGIN android-changed Added -Xmx2G for Mac builds
-java -Xmx2G -jar "$PROGUARD_HOME/lib/proguard.jar" "$@"
+java -jar "$PROGUARD_HOME/lib/proguard.jar" "$@"
 # END android-changed
